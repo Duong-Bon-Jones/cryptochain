3d78a7737fd8efce025689b54ff64536650043e7
Duong-Bon-Jones <iamtdthewinner@gmail.com>
2021-10-01 16:34:41 +0700

    fix typo

--- src/pages/CryptoDetails.jsx
+++ src/pages/CryptoDetails.jsx
@@ -110,7 +110,7 @@ const CryptoDetails = () => {
                     {cryptoDetails.name} ({cryptoDetails.slug}) Price
                 </Title>
                 <p>
-                    {cryptoDetails.name} live price in USD View value.
+                    {cryptoDetails.name} live price in USD. View value,
                     statistics, market cap and supply.
                 </p>
             </Col>
